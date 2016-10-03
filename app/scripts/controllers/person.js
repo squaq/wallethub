08a0ed9fa7aa02e8eafc69c83bf92a4e5943eb12
TissTech - Danilo S <tisstech@Mac-mini-de-TissTech.local>
2016-10-03 16:34:54 -0300

    sum of notes

--- app/scripts/controllers/person.js
+++ app/scripts/controllers/person.js
@@ -3,7 +3,31 @@
 angular.module('wallethubApp').controller('PersonCtrl', function ($scope, $stateParams, shareData, $location) {
     
     $scope.person = shareData.selectedPersona;
+    $scope.yourRate = 0;
+    $scope.total = $scope.person.data.rate1 + $scope.person.data.rate2 + $scope.yourRate;
     
     $scope.backClick = function(){$location.path('/list');}
+    
+    $scope.validInputRate = function(){
+        if($scope.yourRate < 0 ){
+            $scope.yourRate = 10;
+        }
+        if($scope.yourRate > 10 ){
+            $scope.yourRate = 0;
+        }
+        
+        $scope.total = $scope.person.data.rate1 + $scope.person.data.rate2 + $scope.yourRate;
+    }
+    
+    $scope.validInputTotal = function(){
+        if($scope.total > ($scope.person.data.rate1 + $scope.person.data.rate2 + 10) ){
+            $scope.total = ($scope.person.data.rate1 + $scope.person.data.rate2);
+        }
+        if($scope.total < ($scope.person.data.rate1 + $scope.person.data.rate2)){
+            $scope.total = ($scope.person.data.rate1 + $scope.person.data.rate2);
+        }
+        
+        $scope.yourRate = $scope.total - ($scope.person.data.rate1 + $scope.person.data.rate2)
+    }
     
 });
